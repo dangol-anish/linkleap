010ad0386e76df29afae5aa61b5d21d5b9f3d14a
Anish Dangol <dangol.anish001@gmail.com>
2024-05-15 17:40:43 +0545

    updated the add customer controller

--- backend/controllers/customer.controller.js
+++ backend/controllers/customer.controller.js
@@ -13,15 +13,13 @@ async function logCustomerActivity(
   const loggerQuery =
     "insert into customer_logs (customer_id, event_type, old_value, new_value, changed_by) values ($1, $2, $3, $4, $5) ";
 
-  const loggerResult = await pool.query(loggerQuery, [
+  await pool.query(loggerQuery, [
     customerId,
     eventType,
     oldValue,
     newValue,
     changedBy,
   ]);
-
-  console.log("loggerResult", loggerResult);
 }
 
 const addCustomerData = async (req, res, next) => {
@@ -38,7 +36,7 @@ const addCustomerData = async (req, res, next) => {
 
   try {
     const checkExistingCustomerQuery =
-      "select customer_id from customers where customer_email = $1";
+      "SELECT customer_id FROM customers WHERE customer_email = $1";
 
     const checkExistingCustomerResult = await pool.query(
       checkExistingCustomerQuery,
@@ -51,27 +49,38 @@ const addCustomerData = async (req, res, next) => {
       );
     }
 
-    const insertNewCustomerDataQuery =
-      "insert into customers (customer_name, customer_email, customer_company, customer_job_title, customer_deal_value, customer_description, customer_status) values($1, $2, $3, $4, $5, $6, $7) returning customer_id";
+    const insertNewCustomerQuery = `INSERT INTO customers 
+        (customer_name, customer_email, customer_company, customer_job_title, customer_deal_value, customer_description, customer_status) 
+       VALUES ($1, $2, $3, $4, $5, $6, $7) 
+       RETURNING customer_id`;
 
-    const insertNewCustomerDataResult = await pool.query(
-      insertNewCustomerDataQuery,
-      [
-        customerName,
-        customerEmail,
+    const insertNewCustomerResult = await pool.query(insertNewCustomerQuery, [
+      customerName,
+      customerEmail,
+      customerCompany,
+      customerJobTitle,
+      customerDealValue,
+      customerDescription,
+      customerStatus,
+    ]);
+
+    const customerId = insertNewCustomerResult.rows[0].customer_id;
+
+    if (insertNewCustomerResult.rowCount > 0) {
+      const getCompanyIdQuery =
+        "SELECT company_id FROM companies WHERE company_name = $1";
+
+      const getCompanyIdResult = await pool.query(getCompanyIdQuery, [
         customerCompany,
-        customerJobTitle,
-        customerDealValue,
-        customerDescription,
-        customerStatus,
-      ]
-    );
+      ]);
 
-    const returningCustomerId = insertNewCustomerDataResult.rows[0].customer_id;
+      const companyId = getCompanyIdResult.rows[0].company_id;
 
-    console.log("Customer Id", returningCustomerId);
+      const assignCustomerToCompanyQuery =
+        "INSERT INTO company_customers (company_id, customer_id) VALUES ($1, $2)";
 
-    if (insertNewCustomerDataResult.rowCount > 0) {
+      await pool.query(assignCustomerToCompanyQuery, [companyId, customerId]);
+
       res.status(200).json({
         success: true,
         message: "New Customer Added!",
@@ -79,7 +88,7 @@ const addCustomerData = async (req, res, next) => {
     }
 
     await logCustomerActivity(
-      returningCustomerId,
+      customerId,
       "INSERT",
       null,
       "Customer Added",
