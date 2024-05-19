54087b9eeb237b748e7040c60bf630b97763ad07
Anish Dangol <dangol.anish001@gmail.com>
2024-05-19 22:46:27 +0545

    .

--- frontend/src/pages/CompanyDetails.jsx
+++ frontend/src/pages/CompanyDetails.jsx
@@ -63,56 +63,55 @@ const CompanyDetails = () => {
 
   return (
     <>
-      <main className="flex min-h-screen">
+      <main className="flex flex-col lg:flex-row min-h-screen">
         <Sidebar />
 
         <section className="w-full flex flex-col">
-          <header className="flex w-full py-[32px] px-[32px] border-b-[1px] ">
+          <header className="flex w-full py-8 px-8 lg:py-16 lg:px-16 border-b">
             <div>
-              <h2 className="text-[30px] font-medium">Companies</h2>
+              <h2 className="text-2xl lg:text-3xl font-medium">Companies</h2>
               <p className="text-linkleap-gray">
                 Track, manage, and forecast your customer and orders.
               </p>
             </div>
           </header>
-          <div className="flex h-full w-full ">
-            <div className="w-[30%] py-[16px] px-[32px] flex flex-col gap-[24px] border-r-[1px] border-linkleap-border">
-              <label className="flex flex-col gap-[6px] w-full" htmlFor="">
-                <p className="text-[14px] font-medium">Company Name</p>
-                <p className="w-full focus:border-linkleap-login-btn focus:outline-none px-[14px] py-[10px] border-[1px] rounded-[8px]">
+          <div className="flex flex-col lg:flex-row h-full w-full">
+            <div className="w-full lg:w-1/3 py-4 px-8 flex flex-col gap-6 border-b lg:border-r lg:border-b-0 border-linkleap-border">
+              <label className="flex flex-col gap-1 w-full">
+                <p className="text-sm font-medium">Company Name</p>
+                <p className="w-full px-3 py-2 border rounded">
                   {currentCompanyData.companyName}
                 </p>
               </label>
-              <label className="flex flex-col gap-[6px] w-full" htmlFor="">
-                <p className="text-[14px] font-medium">Website</p>
-                <p className="w-full focus:border-linkleap-login-btn focus:outline-none px-[14px] py-[10px] border-[1px] rounded-[8px]">
+              <label className="flex flex-col gap-1 w-full">
+                <p className="text-sm font-medium">Website</p>
+                <p className="w-full px-3 py-2 border rounded">
                   {currentCompanyData.companyWebsite}
                 </p>
               </label>
-              <label className="w-full flex flex-col gap-[6px]" htmlFor="">
-                <p className="text-[14px] font-medium">
-                  Company Description Title
-                </p>
-                <p className="w-full focus:border-linkleap-login-btn focus:outline-none px-[14px] py-[10px] border-[1px] rounded-[8px]">
+              <label className="w-full flex flex-col gap-1">
+                <p className="text-sm font-medium">Company Description Title</p>
+                <p className="w-full px-3 py-2 border rounded">
                   {currentCompanyData.companyDescTitle}
                 </p>
               </label>
-              <label className="flex flex-col gap-[6px] w-full" htmlFor="">
-                <p className="text-[14px] font-medium">Company Description</p>
-                <p className="w-full focus:border-linkleap-login-btn focus:outline-none px-[14px] py-[10px] border-[1px] rounded-[8px]">
+              <label className="flex flex-col gap-1 w-full">
+                <p className="text-sm font-medium">Company Description</p>
+                <p className="w-full px-3 py-2 border rounded">
                   {currentCompanyData.companyDesc}
                 </p>
               </label>
             </div>
-            <div className="w-[60%] py-[16px] px-[32px] flex flex-col gap-[24px]">
-              <h2 className="text-[24px] font-medium">People</h2>
-              <ul className="flex flex-col gap-[12px]">
+            <div className="w-full lg:w-2/3 py-4 px-8 flex flex-col gap-6">
+              <h2 className="text-xl lg:text-2xl font-medium">People</h2>
+              <ul className="flex flex-col gap-3">
                 {customersList.map((data) => (
-                  <li className="flex flex-col gap-[6px] w-full">
-                    <p className="text-[14px] font-medium">
-                      {data.customer_name}
-                    </p>
-                    <p className="text-[14px] font-medium text-linkleap-gray">
+                  <li
+                    key={data.customer_email}
+                    className="flex flex-col gap-1 w-full"
+                  >
+                    <p className="text-sm font-medium">{data.customer_name}</p>
+                    <p className="text-sm font-medium text-linkleap-gray">
                       {data.customer_email}
                     </p>
                   </li>
