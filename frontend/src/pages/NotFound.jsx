d82616461b6e6f787bde7681a77006348ff53f69
Anish Dangol <dangol.anish001@gmail.com>
2024-05-20 20:18:53 +0545

    added styling to 404 page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
+    <div className="h-screen flex justify-center flex-col items-center gap-[12px]">
+      <h1 className="text-[96px]"> 404 </h1>
+      <p className="text-[24px]">Looks like this page doesn't exist!</p>
+      <p className="text-[16px]">
+        Go back to dashboard and continue exploring.
+      </p>
+      <Link
+        to="/dashboard"
+        className="bg-linkleap-login-btn p-2 rounded-[8px] text-white"
+      >
+        Back to Dashboard
+      </Link>
     </div>
   );
 };
