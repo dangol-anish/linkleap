c36ee561fa3d0a06d024a7e40f6b1692847d0f7d
Anish Dangol <dangol.anish001@gmail.com>
2024-05-20 22:14:21 +0545

    superadmin creation test on vercel

--- backend/index.js
+++ backend/index.js
@@ -54,15 +54,15 @@ app.listen(process.env.PORT, () => {
 
 // superadmin creation and postgres query connection example
 
-// app.post("/admin", async (req, res) => {
-//   const { userName, userDisplayName, userEmail, userPassword, userType } =
-//     req.body;
+app.post("/admin", async (req, res) => {
+  const { userName, userDisplayName, userEmail, userPassword, userType } =
+    req.body;
 
-//   const hashedPassword = bcryptjs.hashSync(userPassword, 10);
-//   const query = await pool.query(
-//     "insert into users (user_name, user_display_name, user_email, user_password, user_type) values($1, $2, $3, $4, $5)",
-//     [userName, userDisplayName, userEmail, hashedPassword, userType]
-//   );
+  const hashedPassword = bcryptjs.hashSync(userPassword, 10);
+  const query = await pool.query(
+    "insert into users (user_name, user_display_name, user_email, user_password, user_type) values($1, $2, $3, $4, $5)",
+    [userName, userDisplayName, userEmail, hashedPassword, userType]
+  );
 
-//   res.json(query);
-// });
+  res.json(query);
+});
