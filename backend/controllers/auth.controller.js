70c9b503bb94cb738f7f483c64bde066ff653392
Anish Dangol <dangol.anish001@gmail.com>
2024-05-21 11:17:29 +0545

    changed same site

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -64,7 +64,12 @@ const login = async (req, res, next) => {
 
 const logout = async (req, res) => {
   try {
-    res.clearCookie("accessToken");
+    res.clearCookie("accessToken", {
+      httpOnly: true, // Ensures the cookie is sent only over HTTP(S), not accessible to JavaScript
+      secure: true, // Ensures the cookie is sent only over HTTPS
+      sameSite: "None", // Allows cross-site cookies
+      path: "/", // Ensures the cookie is sent for all paths
+    });
     res.status(200).json({
       success: true,
       message: "Successfully logged out!",
